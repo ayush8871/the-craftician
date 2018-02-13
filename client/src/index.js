449abd36fef59f22d42b6731b2c4b9cebdcf269d
Ayush Srivastav <catchmrshortcircuit@gmail.com>
2018-02-14 00:57:12 +0530

    Basic React and SASS BoilerPlate

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style/main.scss';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>HellowW</h1>
+    , document.getElementById('root'));
+registerServiceWorker();
